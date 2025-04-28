2ac3f291537732d20a18829cd374cd384f7703b4
Jmtrivinop <jcarrancho10@gmail.com>
2025-04-28 12:13:53 -0500

    Correcion PWA

--- vite.config.js
+++ vite.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
     },
     workbox:{
       globPatterns:['**/*.{js,css,html,png,jpg,jpeg,svg}'],
+      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
     }
   })],
   resolve: { 
